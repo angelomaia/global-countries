012c8fd666de720e4dbb8ae7bfe3fd661e9d88e1
Angelo Maia <angelo.maia@rebase.com.br>
2024-10-14 14:06:05 -0300

    test: add filtering e2e test

---
+++ cypress/e2e/countries_filter.cy.ts
@@ -0,0 +1,23 @@
+describe('countries filter', () => {
+  it('filters by country name', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('input[type="text"]').type('Brazil');
+
+    cy.get('.country-card')
+    .should('exist')
+    .should('be.visible')
+    .should('have.length.greaterThan', 0)
+    .should('include.text', 'Brazil');
+  });
+
+  it('filters by capital name', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('input[type="text"]').type('Buenos Aires');
+
+    cy.get('.country-card')
+    .should('exist')
+    .should('be.visible')
+    .should('have.length.greaterThan', 0)
+    .should('include.text', 'Argentina');
+  })
+})
